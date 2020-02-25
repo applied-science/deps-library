4b5258062ceb3ed205a14ece6ffe29e8974a41c1
Matthew Huebert <mhuebert@gmail.com>
2020-02-25 14:30:49 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# deps-lib
+
+Create a `release.edn` file in your project root, eg:
+
+```clj
+{:group-id "mhuebert"
+ :artifact-id "deps-lib"
+ :scm-url "https://github.com/mhuebert/deps-lib"}
+```
+
+Add a `:release` alias to your `deps.edn` as follows:
+
+```clj
+:aliases
+ {:release
+  {:extra-deps {mhuebert/deps-lib {:local/root "../deps-lib"}}
+   :main-opts ["-m" "deps-lib.release"]}}
+```
+
+Create an initial version tag (if you haven't already)
+
+```
+git tag v0.1.0
+```
+
+To bump versions:
+```
+clj -A:release tag <patch, minor, major>
+```
+
+To deploy:
+```
+clj -A:release
+```
---
+++ src/deps_lib/release.clj
@@ -0,0 +1,51 @@
+(ns deps-lib.release
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.java.shell :as sh]
+            [deps-deploy.deps-deploy :as deps-deploy]
+            [garamond.main :as garamond]
+            [garamond.git :as git]
+            [garamond.pom :as pom]
+            [taoensso.timbre :as timbre]
+            [mach.pack.alpha.skinny :as skinny]))
+
+(def default-opts
+  {:jar-dir "target"
+   :jar-name "project.jar"})
+
+(defn garamond-args [{:keys [group-id
+                             artifact-id
+                             scm-url]}]
+  ["--group-id" group-id
+   "--artifact-id" artifact-id
+   "--scm-url" scm-url])
+
+(defn jar [{:keys [jar-dir
+                   jar-name]} & args]
+  (apply skinny/-main (concat args ["--no-libs" "--project-path" (str jar-dir "/" jar-name)])))
+
+(defn pom [opts & args]
+  (apply garamond/-main (concat (garamond-args opts) ["--pom"] args)))
+
+(defn tag [opts & args]
+  (apply garamond/-main (concat (garamond-args opts) ["--tag"] args)))
+
+(defn -main [& [command & args]]
+  (let [{:as opts
+         :keys [jar-dir
+                jar-name]} (-> (io/file "release.edn")
+                               (slurp)
+                               (edn/read-string)
+                               (->> (merge default-opts)))]
+    (case command
+      "build-deploy" (do
+                       (timbre/set-level! :warn)
+                       (pom/generate! (:version (git/current-status)) opts)
+                       (jar opts)
+                       (sh/sh "cp" "pom.xml" jar-dir)
+                       (apply deps-deploy/-main ["deploy" (str jar-dir "/" jar-name)]))
+      "jar" (apply jar opts args)
+      "pom" (apply pom opts args)
+      "tag" (apply tag opts args)
+      "deploy" (apply deps-deploy/-main (concat ["deploy"] args [(str jar-dir "/" jar-name)]))
+      nil (-main "build-deploy"))))
